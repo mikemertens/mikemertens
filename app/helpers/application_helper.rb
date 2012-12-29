09564d552551f7da439b54b6f439eb74873d80a1
Michael Mertens <mikemertens@gmail.com>
2012-12-28 19:47:22 -0500

    new title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Michael Mertens, Experience Designer and Product Manager"
+    base_title = "Michael Mertens | Hand-Crafted Product Strategy, Experience Design and Product Management "
     	if page_title.empty?
 		  base_title
 		else
